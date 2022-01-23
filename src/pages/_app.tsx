be714a9ec8f5feb01c878815e8d53954473e3a7b
Andrêves Dickow <andrevesdickow@MacBook-Air-de-Andreves.local>
2022-01-23 15:48:36 -0300

    desenvolvimento do desafio.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import Header from '../components/Header';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
